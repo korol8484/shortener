bfe20378b9dae420e9d1de7ad8f5ae4564a42c8f
Черкашин Н <n.cherkashin@etpgpb.ru>
2024-11-25 22:11:43 +0300

    Инкремент 23

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/korol8484/shortener/internal/app/config"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"os"
+	"path"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func Test_run(t *testing.T) {
+	os.Clearenv()
+
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		_ = syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}()
+
+	err := run(&config.App{HTTPS: &config.HTTPS{}, Listen: ":8099"}, zap.L())
+	require.NoError(t, err)
+
+	err = run(&config.App{HTTPS: &config.HTTPS{}, Listen: "8099"}, zap.L())
+	require.Error(t, err)
+
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		_ = syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}()
+
+	_ = os.Setenv("SERVER_ADDRESS", ":8098")
+	_ = os.Setenv("ENABLE_HTTPS", "1")
+	_ = os.Setenv("FILE_STORAGE_PATH", path.Join(os.TempDir(), "/db"))
+
+	cfg, err := config.NewConfig()
+	require.NoError(t, err)
+
+	err = run(cfg, zap.L())
+	require.NoError(t, err)
+
+	_ = os.Remove(cfg.HTTPS.Key)
+	_ = os.Remove(cfg.HTTPS.Pem)
+}
--- internal/app/storage/db/db_store_test.go
+++ internal/app/storage/db/db_store_test.go
@@ -1,29 +1,184 @@
 package db
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/korol8484/shortener/internal/app/domain"
+	"github.com/korol8484/shortener/internal/app/storage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
-func TestNewStorage(t *testing.T) {
-	db, mock, err := sqlmock.New()
+var (
+	db    *sql.DB
+	mock  sqlmock.Sqlmock
+	store *Storage
+)
+
+func TestMain(m *testing.M) {
+	code, err := run(m)
 	if err != nil {
-		t.Fatal(fmt.Errorf("an error '%w' was not expected when opening a stub database connection", err))
+		fmt.Println(err)
 	}
-	defer db.Close()
+
+	err = store.Close()
+	if err == nil {
+		fmt.Println(err)
+	}
+
+	os.Exit(code)
+}
+
+func run(m *testing.M) (int, error) {
+	var err error
+	db, mock, err = sqlmock.New()
 
 	mock.ExpectBegin()
 	mock.ExpectExec("create table if not exists shortener .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("create index .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("create unique .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec("alter table shortener.*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectExec("alter table shortener .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("create table if not exists user_url.*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectExec("create unique index.*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
 	mock.ExpectCommit()
 
-	_, err = NewStorage(db)
+	store, err = NewStorage(db)
 	if err != nil {
-		t.Fatal(err)
+		return -1, err
 	}
+
+	return m.Run(), nil
+}
+
+func TestStorage_Add(t *testing.T) {
+	mock.ExpectBegin()
+	mock.ExpectQuery("INSERT INTO shortener").
+		WithArgs("1", "1").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectExec("INSERT INTO user_url").
+		WithArgs(1, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	err := store.Add(context.Background(), &domain.URL{URL: "1", Alias: "1"}, &domain.User{ID: 1})
+	require.NoError(t, err)
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("INSERT INTO shortener").
+		WithArgs("2", "2").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(0))
+
+	mock.ExpectQuery("SELECT t.id FROM shortener t").
+		WithArgs("2").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(2))
+
+	mock.ExpectExec("INSERT INTO user_url").
+		WithArgs(2, 2).
+		WillReturnResult(sqlmock.NewResult(2, 1))
+	mock.ExpectCommit()
+
+	err = store.Add(context.Background(), &domain.URL{URL: "2", Alias: "2"}, &domain.User{ID: 2})
+	require.ErrorIs(t, err, storage.ErrIssetURL)
+}
+
+func TestStorage_Read(t *testing.T) {
+	mock.ExpectQuery("SELECT t.url, t.alias, t.deleted FROM shortener t").
+		WithArgs("alias").
+		WillReturnRows(
+			sqlmock.NewRows([]string{"url", "alias", "deleted"}).
+				AddRow("http://ya.ru", "alias", false),
+		)
+
+	url, err := store.Read(context.Background(), "alias")
+	require.NoError(t, err)
+
+	assert.Equal(t, "http://ya.ru", url.URL)
+	assert.Equal(t, "alias", url.Alias)
+	assert.False(t, url.Deleted)
+
+	mock.ExpectQuery("SELECT t.url, t.alias, t.deleted FROM shortener t").
+		WithArgs("alias").WillReturnError(sql.ErrNoRows)
+
+	_, err = store.Read(context.Background(), "alias")
+	require.ErrorIs(t, err, sql.ErrNoRows)
+}
+
+func TestStorage_ReadByURL(t *testing.T) {
+	mock.ExpectQuery("SELECT t.url, t.alias FROM shortener t").
+		WithArgs("http://ya.ru").
+		WillReturnRows(
+			sqlmock.NewRows([]string{"url", "alias"}).
+				AddRow("http://ya.ru", "alias"),
+		)
+
+	url, err := store.ReadByURL(context.Background(), "http://ya.ru")
+	require.NoError(t, err)
+
+	assert.Equal(t, "http://ya.ru", url.URL)
+	assert.Equal(t, "alias", url.Alias)
+	assert.False(t, url.Deleted)
+}
+
+func TestStorage_ReadUserURL(t *testing.T) {
+	mock.ExpectQuery("SELECT s.url, s.alias FROM shortener s").
+		WithArgs(1).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"url", "alias"}).
+				AddRow("http://ya.ru", "alias"),
+		)
+
+	url, err := store.ReadUserURL(context.Background(), &domain.User{ID: 1})
+	require.NoError(t, err)
+
+	assert.Len(t, url, 1)
+}
+
+func TestStorage_BatchDelete(t *testing.T) {
+	mock.ExpectExec("UPDATE shortener s SET deleted = true").
+		WithArgs(1, "alias").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := store.BatchDelete(context.Background(), []string{"alias"}, 1)
+	require.NoError(t, err)
+}
+
+func TestStorage_AddBatch(t *testing.T) {
+	mock.ExpectBegin()
+	mock.ExpectQuery("INSERT INTO shortener").
+		WithArgs("1", "1").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectExec("INSERT INTO user_url").
+		WithArgs(1, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	err := store.AddBatch(context.Background(), domain.BatchURL{
+		&domain.URL{URL: "1", Alias: "1"},
+	}, &domain.User{ID: 1})
+	assert.NoError(t, err)
+}
+
+func TestStorage_Close(t *testing.T) {
+	cDb, CMock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	CMock.ExpectBegin()
+	CMock.ExpectExec("create table if not exists shortener .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectExec("create index .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectExec("create unique .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectExec("alter table shortener .*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectExec("create table if not exists user_url.*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectExec("create unique index.*").WithoutArgs().WillReturnResult(sqlmock.NewResult(1, 1))
+	CMock.ExpectCommit()
+
+	cStore, err := NewStorage(cDb)
+	require.NoError(t, err)
+
+	CMock.ExpectClose()
+	err = cStore.Close()
+	require.NoError(t, err)
 }
